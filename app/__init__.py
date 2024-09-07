a8e7fc7d97b25caaa9579023b6604f7e882659aa
kevin <kevin@novek.io>
2024-09-07 14:19:12 +0300

    feat:added dependancies

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from app.config import Config
 from app.models.blockchain import Blockchain
 from app.routes.blockchain_routes import blockchain_bp
 from app.routes.node_routes import node_bp
+from app.utils.persistence import load_blockchain
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     CORS(app)
 
-    app.blockchain = Blockchain()
+    app.blockchain = Blockchain.load_or_create()
 
     app.register_blueprint(blockchain_bp)
     app.register_blueprint(node_bp)
