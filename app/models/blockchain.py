66112c66d7789a43a2d2eac1d8406bb2df8b96df
kevin <kevin@novek.io>
2024-09-07 14:19:43 +0300

    feat:decoupled block consensus algorith

--- app/models/blockchain.py
+++ app/models/blockchain.py
@@ -1,6 +1,10 @@
 import time
+from app.utils.persistence import save_blockchain, load_blockchain
+from app.utils.merkle_tree import MerkleTree
 from app.utils.crypto import calculate_hash
 from app.utils.validation import is_valid_proof
+from app.models.transaction import Transaction, TXInput, TXOutput
+from app.utils.wallet import Wallet
 
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
@@ -8,21 +12,28 @@ class Block:
         self.transactions = transactions
         self.timestamp = timestamp
         self.previous_hash = previous_hash
+        self.merkle_root = MerkleTree(transactions).get_root() if transactions else None
         self.nonce = nonce
         self.hash = calculate_hash(self)
+
 
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.utxo_set = {}
+        self.wallets = {}
         self.pending_transactions = []
         self.nodes = set()
-        self.create_genesis_block()
         self.difficulty = 4
+        self.create_genesis_block()
 
     def create_genesis_block(self):
         genesis_block = Block(0, [], int(time.time()), "0")
         genesis_block.hash = calculate_hash(genesis_block)
         self.chain.append(genesis_block)
+
+    def save(self):
+        save_blockchain(self)
 
     @property
     def last_block(self):
@@ -46,6 +57,10 @@ class Blockchain:
         })
         return self.last_block.index + 1
 
+    def create_transaction(self, sender, recipient, amount):
+        # Implement transaction creation using UTXO model
+        pass
+
     def mine(self):
         if not self.pending_transactions:
             return False
@@ -68,6 +83,15 @@ class Blockchain:
             block.nonce += 1
             computed_hash = calculate_hash(block)
         return computed_hash
+
+    def get_balance(self, address):
+        # Implement balance calculation using UTXO set
+        pass
+
+    def create_wallet(self):
+        wallet = Wallet()
+        self.wallets[wallet.address] = wallet
+        return wallet.address
 
     def register_node(self, address):
         self.nodes.add(address)
